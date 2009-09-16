b7da7e5df5ff7241e4d8150f323d4c5a2ce03a6e
Jamie Wilkinson <jamie@tramchase.com>
2009-09-16 13:06:00 -0700

    continuous building of the final output movie. Transcodes all to 640x480,15FPS intermediate MPEG, concats together, then re-encodes (generically)

--- swu-ci.sh
+++ swu-ci.sh
@@ -1,13 +1,39 @@
 #!/bin/sh
+#
+# Continuous Build for Star Wars Uncut
+# in effect... this script remakes Star Wars!!11
+# Jamie Wilkinson <http://jamiedubs.com> @jamiew github:jamiew
+#
+
+# tuned for my server
 cd /home/jamie/vimeo-ripper
+RUBY="/usr/bin/env ruby"
 
 # update
-/opt/ruby-enterprise/bin/ruby swu-compiler.rb
-/opt/ruby-enterprise/bin/ruby vimeo-ripper.rb
+echo "Contacting SWU HQ for list of uploads ..."
+$RUBY swu-compiler.rb
 
-# roll up
-cd output
-rm -f /00_daily_dump.tgz
-tar czvf 00_daily_dump.tgz *
+echo "Fetching latest videos ..."
+$RUBY vimeo-ripper.rb
 
-# TODO: also generate a stitched video :)
+# echo "Rolling up dump of individual video files ..."
+# rm -f output/00_daily_dump.tgz
+# tar czvf output/00_daily_dump.tgz output/*
+
+# transcode to intermediate format
+for i in output/*; do
+  echo "Transcoding $i ..."
+  ffmpeg -y -r 15 -sameq -s 640x480 -aspect 4:3 -i "$i" "intermediate/$(basename $i).mpg" #FIXME: ending up w/ .avi.mpg -- it's fine tho
+done
+
+# generate a stitched video
+mkdir -f render
+echo "Merging transcoded videos..."
+cat intermediate/* > "render/merged.mpg"
+
+OUTPUT="render/merged.mov"
+echo "Re-encoding as $OUTPUT ..."
+ffmpeg -i "render/merged.mpg" "$OUTPUT"
+
+echo "Done! May the Force be with you."
+exit
